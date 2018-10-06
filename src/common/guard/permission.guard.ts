d9bd512b0b074eb34bae014902b016ff28a01865
lxzgg <471227142@qq.com>
2018-10-06 12:58:46 +0800

    update package.json

--- src/common/guard/permission.guard.ts
+++ src/common/guard/permission.guard.ts
@@ -1,4 +1,4 @@
-import {CanActivate, ExecutionContext, Injectable, UnauthorizedException} from '@nestjs/common'
+import {CanActivate, ExecutionContext, Injectable} from '@nestjs/common'
 import {Observable} from 'rxjs'
 import {PERMISSION_DEFINITION} from '../decorator/permission.decorator'
 import {Permission} from '../../entity/permission.entity'
